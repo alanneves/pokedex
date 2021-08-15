e573ea30c1a20d20a86a6d1b304f79c4c8962a59
Alan Neves <alanrenoneves@gmail.com>
2021-08-15 00:34:21 -0300

    chore: init project

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="state.searchFilter" />
+    {{ state.searchFilter }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+
+  data() {
+    return {
+      state: {
+        searchFilter: '',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
